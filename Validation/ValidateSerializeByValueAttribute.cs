41faf57906336eb8c4816ab51013f54682eecfec
Exanite <exanite78012@gmail.com>
2024-05-10 23:38:43 -0400

    Add remark doc comment

--- Validation/ValidateSerializeByValueAttribute.cs
+++ Validation/ValidateSerializeByValueAttribute.cs
@@ -6,6 +6,9 @@ namespace CodeName.Serialization.Validation
     /// Used to prevent accidentally serializing objects by value.
     /// If a property or field containing the object is serialized, the property or field must have the <see cref="SerializeByValueAttribute"/> applied.
     /// </summary>
+    /// <remarks>
+    /// This is used in CodeName Prototype to prevent accidentally directly serializing an EntityInstance when serializing the EntityId was intended.
+    /// </remarks>
     [AttributeUsage(AttributeTargets.Class)]
     public class ValidateSerializeByValueAttribute : Attribute {}
 }
